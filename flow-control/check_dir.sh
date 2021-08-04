9582b507a1e82f1f32661d85ac4f7744dba095db
Juan <pront23@gmail.com>
2021-08-04 14:08:35 +0900

    flow control section complete

---
+++ flow-control/check_dir.sh
@@ -0,0 +1,13 @@
+# Test example 1
+if [ -d "/home/bob/caleston" ]; then
+    echo "Directory \"/home/bob/caleston\" exists"
+else
+    echo "Directory not found"
+fi
+
+# Test example 2
+if [ -d "/home/bob/caleston" ]; then
+    echo "Directory exists"
+else
+    echo "Directory not found"
+fi
