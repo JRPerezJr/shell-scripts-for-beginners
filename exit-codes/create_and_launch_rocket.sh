0055454d639b25bef72d714f73b03377931b676b
Juan <pront23@gmail.com>
2021-08-07 21:28:56 +0900

    added exit code

---
+++ exit-codes/create_and_launch_rocket.sh
@@ -0,0 +1,28 @@
+# Add exit code 25 to script
+mission_name=$1
+
+mkdir $mission_name
+
+rocket-add $mission_name
+
+rocket-start-power $mission_name
+rocket-internal-power $mission_name
+rocket-start-sequence $mission_name
+rocket-start-engine $mission_name
+rocket-lift-off $mission_name
+
+rocket_status=$(rocket-status $mission_name)
+
+echo "The status of launch is $rocket_status"
+
+if [ $rocket_status = "launching" ]
+then
+  sleep 2
+  rocket_status=$(rocket-status $mission_name)
+fi
+
+if [ $rocket_status = "failed" ]
+then
+  rocket-debug
+  exit 25
+fi
